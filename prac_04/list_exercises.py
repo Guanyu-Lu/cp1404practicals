3eafa803b9a6348f95f8d328ef7bd1c00a9372b7
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-10-07 19:15:46 +0800

    Finish part of homework for practical04 in CP1404

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,26 @@
+#Part1
+NUMBER_COUNT=5
+numbers=[]
+for number in range(NUMBER_COUNT):
+    is_integer=False
+    while not is_integer:
+        try:
+            number=int(input("Number:"))
+            numbers.append(number)
+            is_integer=True
+        except ValueError:
+            print("Please enter an integer.")
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[4]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+#Part2
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username=input("Username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
---
+++ prac_04/subject_reader.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+FILENAME = "subject_data.txt"
+
+
+def main():
+    """Loads subject data from a file and prints a formatted report."""
+    datas = load_data(FILENAME)
+    print(datas)
+    print_subject_details(datas)
+
+
+def load_data(filename=FILENAME):
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    with open(filename,"r") as input_file:
+        datas=[line.strip("\n").split(",") for line in input_file]
+    for data in datas:
+        data[2] = int(data[2])
+    return datas
+
+
+def  print_subject_details(datas):
+    """ print subject details from the data file."""
+    for data in datas:
+        print(f"{data[0]} is taught by {data[1]:12} and has {data[2]:>3} students")
+
+
+main()
