1b79dbd865566bf9009e04e8f26e4a33e6017df9
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-10-12 11:22:09 +0800

    Increase the error checking in total_income.py.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -11,11 +11,39 @@ def main():
 def get_incomes():
     """Get monthly incomes based on number of months from user input."""
     incomes = []
-    number_of_months = int(input("How many months? "))
+    number_of_months = get_valid_months()
     for month in range(1, number_of_months + 1):
-        income = float(input(f"Enter income for month {month} : "))
+        income = get_valid_incomes(month)
         incomes.append(income)
     return incomes
+
+def get_valid_months():
+    """Get valid number of months based on user input."""
+    is_integer = False
+    while not is_integer:
+        try:
+            number_of_months = int(input("How many months? "))
+            if number_of_months >=0:
+                is_integer = True
+            else:
+                print("Please enter a positive integer.")
+        except ValueError:
+            print("Please enter an integer.")
+    return number_of_months
+
+def get_valid_incomes(month):
+    """Get valid incomes based on user input."""
+    is_float = False
+    while not is_float:
+        try:
+            income = float(input(f"Enter income for month {month} : "))
+            if income >=0:
+                is_float = True
+            else:
+                print("Please enter a positive float.")
+        except ValueError:
+            print("Please enter an float.")
+    return income
 
 def print_report(incomes):
     """Print income report including monthly income and total income"""
