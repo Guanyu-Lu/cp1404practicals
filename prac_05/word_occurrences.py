edf62cf11ee120d005467b5a1890310c333ba38c
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-10-19 11:55:53 +0800

    Change the name of the dictionary to make it easier to understand and use get to simplify the code in word_occurrences.py.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,16 +3,13 @@ Word Occurrences
 Estimate: 20 minutes
 Actual:   18 minutes
 """
-dictionary={}
+word_to_count={}
 texts=input("text: ").lower().strip().split(" ")
 texts.sort()
 print(texts)
 for word in texts:
-    if word not in dictionary.keys():
-        dictionary[word]=1
-    else:
-        dictionary[word]+=1
-print(dictionary)
-word_width=max(len(word) for word in dictionary.keys())
-for word,num in dictionary.items():
+    word_to_count[word] = word_to_count.get(word, 0) + 1
+print(word_to_count)
+word_width=max(len(word) for word in word_to_count.keys())
+for word,num in sorted(word_to_count.items()):
     print(f"{word:{word_width+1}}: {num}")
