284c3947e9384ff0c4b19a69c1527ceefea812d2
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-10-19 12:10:08 +0800

    Add a new function to confirm name in emails.py.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -11,13 +11,19 @@ def main():
     email = input("Email:").strip()
     while email != "":
         name = extract_name(email)
-        confirmation = input(f"Is your name {name}? (Y/n)").upper()
-        if confirmation == "N" or confirmation == "NO":
-            name = input("Name:").strip()
+        name = confirm_name(name)
         email_to_name[name] = email
         email = input("Email:").strip()
     print()
     print_emails(email_to_name)
+
+
+def confirm_name(name):
+    """Confirm name based on the name from email"""
+    confirmation = input(f"Is your name {name}? (Y/n)").upper()
+    if confirmation == "N" or confirmation == "NO":
+        name = input("Name:").strip()
+    return name
 
 
 def extract_name(email):
