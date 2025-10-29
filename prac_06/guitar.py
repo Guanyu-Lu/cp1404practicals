8a628ed4d5fd7445f6f6c11f87da1f0ef0f7d677
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-10-29 21:54:24 +0800

    finish guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,24 @@
+CURRENT_YEAR=2025
+class Guitar:
+    def __init__(self,name="", year=0, cost=0):
+        """
+        Initialise a Guitar instance.
+               name: string, name of the guitar
+               year: integer, The year of guitar making
+               cost: float, the cost of the guitar
+        """
+        self.name=name
+        self.year=year
+        self.cost=cost
+
+    def __str__(self):
+        """Return a string representation of the guitar."""
+        return f"{self.name}({self.year}): ${self.cost:,2f}"
+
+    def get_age(self):
+        """Return the age of the guitar."""
+        return CURRENT_YEAR - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is vintage(more than or equal to 50 years)."""
+        return self.get_age()>=50
