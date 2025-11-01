ff9d2f5f33aeea3e1be077a0cf988eb83aa917b0
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-01 16:19:43 +0800

    Modify some docstrings in programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,12 +5,12 @@ Actual:   9 minutes
 """
 
 class ProgrammingLanguage:
-    """Represent a ProgrammingLanguage object."""
-    def __init__(self, name="", typing="", reflection="", year=0):
+    """Represent a programming language with name, typing, reflection,and first release year."""
+    def __init__(self, name="", typing="", reflection=False, year=0):
         """Initialise a ProgrammingLanguage instance.
         name: string, name of the programming language
         typing: string, type checking method of the programming language
-        reflection: bool, reflection status of the programming language(True=Can reflection, False=Not reflection)
+        reflection: bool, reflection status of the programming language
         year: integer, first release year the programming language
         """
         self.name = name
@@ -23,5 +23,5 @@ class ProgrammingLanguage:
         return self.typing == "Dynamic"
 
     def __str__(self):
-        """Return a f-string representation of the language object."""
+        """Return formatted string representation of the language."""
         return f"{self.name}, {self.typing}, Reflection={self.reflection}, First appeared in {self.year}"
