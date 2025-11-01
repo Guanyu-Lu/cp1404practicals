e23749bbe2b8f5e7dde8e4cf533e027241ccea7b
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-01 15:11:03 +0800

    Make a small change in guitar_test.py Add estimate and actual time in guitar_test.py

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,11 +1,19 @@
+"""
+guitar_test
+Estimate: 10 minutes
+Actual:   6 minutes
+"""
+
 from prac_06.guitar import Guitar
 CURRENT_YEAR=2025
+
+
 def main():
     """Create and test Guitar objects. """
     gibson_guitar = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another_guitar= Guitar("Another Guitar", 2013, 1500)
-    print(f"{gibson_guitar.name} get_age() - Expected {CURRENT_YEAR - gibson_guitar.year}. Got: {gibson_guitar.get_age()}")
-    print(f"{another_guitar.name} get_age() - Expected {CURRENT_YEAR - another_guitar.year}. Got: {another_guitar.get_age()}")
+    print(f"{gibson_guitar.name} get_age() - Expected 103. Got: {gibson_guitar.get_age()}")
+    print(f"{another_guitar.name} get_age() - Expected 12. Got: {another_guitar.get_age()}")
     print(f"{gibson_guitar.name} is_vintage() - Expected True. Got: {gibson_guitar.is_vintage()}")
     print(f"{another_guitar.name} is_vintage() - Expected False. Got: {another_guitar.is_vintage()}")
 
