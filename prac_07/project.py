27ca7f7fc4470cbcee71eee29153c953adc88ed6
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-07 23:02:03 +0800

    Complete init method part of class project.

---
+++ prac_07/project.py
@@ -0,0 +1,14 @@
+"""
+Languages
+Estimate: 20 minutes
+Actual:    minutes
+"""
+class Project:
+    def __init__(self,name,start_date,priority,estimate_cost,completed_percentage):
+        self.name=name
+        self.start_date=start_date
+        self.priority=priority
+        self.estimate_cost=estimate_cost
+        self.completed_percentage=completed_percentage
+
+
