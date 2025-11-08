7e1a3c7dfa892422892fc70cb5e9dbd00e9d577b
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-09 00:22:59 +0800

    Add docstrings for methods in project class.

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,19 @@
 """
 Languages
 Estimate: 20 minutes
-Actual:    minutes
+Actual: 18 minutes
 """
 class Project:
+    """Represent a Project object."""
+
     def __init__(self,name,start_date,priority,estimate_cost,completed_percentage):
+        """Initialise a Project instance.
+        name: str,name of project.
+        start_date: datetime.date object,start date of project.
+        priority: int,priority of project.
+        estimate_cost: float,estimated cost of project.
+        completed_percentage: int,completed percentage of project.
+        """
         self.name=name
         self.start_date=start_date
         self.priority=priority
@@ -12,11 +21,13 @@ class Project:
         self.completed_percentage=completed_percentage
 
     def __str__(self):
+        """Return a formatted string representation of the Project."""
         formatted_date = self.start_date.strftime("%d/%m/%Y")
         return (f"{self.name}, start:{formatted_date}, "
                 f"priority {self.priority}, estimate: ${self.estimate_cost:.2f}, "
                 f"completed_percentage: {self.completed_percentage}%")
 
     def __lt__(self, other):
+        """Define comparison for sorting based on project priority"""
         return self.priority < other.priority
 
