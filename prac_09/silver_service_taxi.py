2979da76685c51120a87f3c46914f4b2ec190748
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-19 08:09:22 +0800

    Finish silver service taxi part.

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,16 @@
+from prac_09.taxi import Taxi
+class SilverServiceTaxi(Taxi):
+
+    def __init__(self,name,fuel,fanciness):
+        super().__init__(name, fuel)
+        self.price_per_km=fanciness*1.23
+        self.current_fare_distance=0
+        self.flagfall=4.5
+
+    def get_fare(self):
+        """Return the price for the taxi trip."""
+        return self.price_per_km * self.current_fare_distance+self.flagfall
+    def __str__(self):
+        """Return a string like a Car but with current fare distance."""
+        return f"{super().__str__()},plus flagfall of ${self.flagfall:.2f}"
+
