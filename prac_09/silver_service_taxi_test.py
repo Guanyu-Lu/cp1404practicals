5e2f687e746cfbd9a726e29ff51b2f4df558d64b
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-20 13:46:53 +0800

    Adjust to retain one decimal place according to the requirements of the question.

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,9 +3,9 @@ print(SilverServiceTaxi("Hummer",100,4))
 taxi=SilverServiceTaxi("SilverServiceTaxi", 100, 2)
 taxi.drive(18)
 current_fare = taxi.get_fare()
-print(current_fare)
+print(f"${current_fare:.2f}")
 try:
-    assert current_fare ==48.78 #Check whether the current_fare meets the expected value
-    print("For an 18 km trip in a with fanciness of 2, the fare should is $48.78.")
+    assert current_fare ==round(48.78,1) #Check whether the current_fare meets the expected value
+    print("For an 18 km trip in a silver service taxi with fanciness of 2, the fare should is $48.80.")
 except AssertionError:
     print("Wrong calculated fare")
