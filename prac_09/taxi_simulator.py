6ef3a40bb50a3cc5dc5ec2f204045866de5c3609
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-21 16:31:34 +0800

    finish choose taxi part of taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,11 +8,23 @@ def main():
     choice=input("{MENU}\n>>>").upper()
     while choice != "Q":
         if choice == "D":
-            if current_taxi = None:
+            if current_taxi == None:
                 print("You need to choose a taxi before you can drive")
             else:
                 distance=float(input("Drive how far?"))
-                print(f"Your Prius trip cost you ${taxis[current_taxi].drive(distance)}")
+                print(f"Your {current_taxi.name} trip cost you ${current_taxi.drive(distance)}")
                 total_bill+=taxis[current_taxi].drive(distance)
                 print(f"Bill to date: ${total_bill:.2f}")
+        elif choice == "C":
+            print("Taxis available:")
+            for i,taxi in enumerate(taxis,0):
+                print(f"{i} - {taxi}")
+        chosen_taxi=int(input("Choose taxi:"))
+        if chosen_taxi < 0 or chosen_taxi >= len(taxis):
+            print("Invalid taxi choice.")
+        else:
+            current_taxi = taxis[chosen_taxi]
+        print(f"Bill to date: ${total_bill:.2f}")
 
+
+main()
