76b37cbf652184fca20811eacd35450c21cb223b
GuanyuLu <guanyu.lu@my.jcu.edu.au>
2025-11-21 17:45:10 +0800

    Add docstrings.

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,19 +1,26 @@
 class Band:
+    """Band class for storing details of a band and its musicians."""
+
     def __init__(self, name):
+        """Construct a Band with a name and empty musician list."""
         self.name = name
         self.musicians = []
 
     def __str__(self):
+        """Return a string representation of a Band."""
         musicians_strings = [str(musician) for musician in self.musicians ]
         return f"{self.name} ({', '.join(musicians_strings)})"
 
     def __repr__(self):
+        """Return a string representation of a band, showing the variables."""
         return  str(vars(self))
 
     def add(self, musician):
+        """Add a musician to the band."""
         self.musicians.append(musician)
 
     def play(self):
+        """Return a string showing the result of playing music by all musicians."""
         if not self.musicians:
             return f"{self.name} needs an musician!"
         else:
--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,12 +1,15 @@
 import random
 from prac_09.car import Car
+
 class UnreliableCar(Car):
 
+    """Unreliable car is a car that can only be driven under certain circumstances."""
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Drive the car a given distance based on reliability."""
         if random.randint(0, 100) < self.reliability:
             distance_driven = super().drive(distance)
         else:
@@ -14,5 +17,5 @@ class UnreliableCar(Car):
         return distance_driven
 
     def __str__(self):
-        """Return a string like a Car but with current fare distance."""
+        """Return a string representation of the UnreliableCar."""
         return super().__str__()
